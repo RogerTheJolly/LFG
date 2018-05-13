110029325511996397b4356745a9781b46a7b814
RogerTheJolly <blobsrule72@yahoo.com>
2018-05-13 16:28:57 -0500

    Angular succesfully loads home page into index page
    It also runs the script, which is pretty important

--- JS/AngularModule.js
+++ JS/AngularModule.js
@@ -29,16 +29,50 @@ app.config(function($routeProvider)
 
 app.controller('HomeController', function($scope)
 {
-    console.log("ANGULAR!");
 	$scope.testText = "Test Text";
 	
-	db.collection("groups").get().then((querySnapshot) => {
+	/*db.collection("groups").get().then((querySnapshot) => {
 		querySnapshot.forEach((doc) => {
 			//console.log(`${doc.id} => ${doc.data()}`);
 			$scope.groupName = doc.id;
 			
 		});
-	});
+	});*/
+	
+	$scope.runJS = true;
+	
+	// Run after view loaded.
+	$scope.loadGroups = function(){
+		if($scope.runJS)
+		{
+			console.log("HOME LAODED");
+			//Get groups and set backgrounds
+			var grid = document.getElementById('grid');
+			
+			//Get the Groups from the DB
+			db.collection("groups").get().then((querySnapshot) => {
+				querySnapshot.forEach((doc) => {
+					//console.log(`${doc.id} => ${doc.data()}`);
+					
+					var item = document.createElement("div");
+					item.classList.add("item");
+					grid.appendChild(item);
+					
+					var image = doc.data().image.toString();
+
+					/*item.addEventListener("click", function(){
+					console.log("clicky");
+						groupPopUp.style.display = "block";
+					});*/
+
+					image = image.replace('300', item.offsetWidth);
+					image = image.replace('200', item.offsetHeight);
+					item.style.backgroundImage = "url('" + image +"')";
+				});
+			});
+			$scope.runJS = false;
+		}
+	};
 });
 
 app.controller('BlogController', function($scope)
